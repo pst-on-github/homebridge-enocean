372903ff4ca745b830e0260eb40c30d083fc5aa7
pst-on-github <stoeckigt@gmx.net>
2025-03-17 20:47:30 +0100

    ops: changed plugin name to contain the account name

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'EnOcean';
 /**
  * This must match the name of your plugin as defined the package.json `name` property
  */
-export const PLUGIN_NAME = 'homebridge-enocean';
+export const PLUGIN_NAME = '@pst-on-npm/homebridge-enocean';
