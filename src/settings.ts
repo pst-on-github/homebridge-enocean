fd53a4251e69b641c7ae9a28261ef30b064bdb51
pst-on-github <stoeckigt@gmx.net>
2025-03-25 22:09:34 +0100

    chore!: PLATFORM_NAME now 'EnOceanPlatform'. Update existing config platforms.platform property

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,11 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
+ * like { "platform": "enOceanPlatform", "name": "EnOcean" }
  */
-export const PLATFORM_NAME = 'EnOcean';
+export const PLATFORM_NAME = 'EnOceanPlatform';
 
 /**
- * This must match the name of your plugin as defined the package.json `name` property
+ * This must match the name of your plugin as defined the package.json `name` property,
+ * including the scope (@pst-on-npm)
  */
 export const PLUGIN_NAME = '@pst-on-npm/homebridge-enocean';
